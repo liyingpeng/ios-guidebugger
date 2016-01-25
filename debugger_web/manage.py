9ea63e90a552c98e0b4056565899cc097c8b9edb
liyingpeng <liyingpeng@baidu.com>
2016-01-25 15:08:18 +0800

    change django to 1.8 version

--- debugger_web/manage.py
+++ debugger_web/manage.py
@@ -1,16 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-################################################################################
-#
-# Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
-#
-################################################################################
-"""
-This module provide configure file management service in i18n environment.
-
-Authors: liyingpeng(liyingpeng@baidu.com)
-Date:    2016/01/20 17:23:06
-"""
 import os
 import sys
 
